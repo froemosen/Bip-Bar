a1be8bc900c2281c4d76f78eeb0500411c7b69d0
nomisa03 <nomisa03@gmail.com>
2022-03-22 09:04:01 +0100

    Files

---
+++ Main.py
@@ -0,0 +1,18 @@
+import tkinter
+from tkinter.tkk import *
+
+class page(tk.frame):
+    def __init__(self, *args, )
+    
+
+    
+
+class serverkoms
+
+
+class nfcreader
+
+
+
+if __name__ == '__main__':
+    main = terminal()
