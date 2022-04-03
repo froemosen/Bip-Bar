ec8b1086b0338dea0b46bf1c2349143753a9632b
froemosen <kristianantonhedegaard@gmail.com>
2022-04-03 20:51:46 +0200

    frame-2022-01-04-10-05-23.jpg....
    Mark looking goood

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from time import sleep
 
 class MainFrame(tk.Frame): 
     def __init__(self, *args, **kwargs):
@@ -6,6 +7,7 @@ class MainFrame(tk.Frame):
 
         Bip_Bar_Window = Bip_Bar(self)
         Edit_User_Window = Edit_User(self)
+        New_User_Window = New_User(self)
 
         #Først laver vi kasser til selve knapperne.
         ButtonFrame = tk.Frame(self, bg = '#bcc8e8', borderwidth = 3, relief = 'raised')
@@ -16,18 +18,21 @@ class MainFrame(tk.Frame):
         #Placering for kasserne
         Bip_Bar_Window.place(in_= Box, x = 0, y = 0, relwidth = 1, relheight = 1)
         Edit_User_Window.place(in_= Box, x = 0, y = 0, relwidth = 1, relheight = 1)
+        New_User_Window.place(in_= Box, x = 0, y = 0, relwidth = 1, relheight = 1)
 
         mainMenu = tk.Label(ButtonFrame, text = "Main Menu", bg = '#bcc8e8')
         mainMenu.config(font=("Courier", 24), bg = '#bcc8e8')
         
         #Selve knapperne bliver lavet
-        Bip_BarButton = tk.Button(ButtonFrame, text = "Bip Bar", height = 16, width = 20, command = Bip_Bar_Window.lift)
-        Edit_UserButton = tk.Button(ButtonFrame, text = "Edit User", height = 16, width = 20, command = Edit_User_Window.lift)
+        Bip_Bar_Button = tk.Button(ButtonFrame, text = "Bip Bar", height = 10, width = 20, command = Bip_Bar_Window.lift)
+        Edit_User_Button = tk.Button(ButtonFrame, text = "Edit User", height = 10, width = 20, command = Edit_User_Window.lift)
+        New_User_Button = tk.Button(ButtonFrame, text = "New User", height = 10, width = 20, command = New_User_Window.lift)
 
         #Knapper formatteres i tabel
         mainMenu.grid(row = 0, column = 0, padx = 0, pady = 15)
-        Bip_BarButton.grid(row = 1, column = 0, padx = 0, pady = 0)
-        Edit_UserButton.grid(row = 2, column = 0, padx = 0, pady = 0)
+        Bip_Bar_Button.grid(row = 1, column = 0, padx = 0, pady = 0)
+        Edit_User_Button.grid(row = 2, column = 0, padx = 0, pady = 0)
+        New_User_Button.grid(row = 3, column = 0, padx = 0, pady = 0)
         
         #Hvilken side programmet skal starte i
         Bip_Bar_Window.show()
@@ -54,6 +59,58 @@ class Bip_Bar(page):
 class Edit_User(page):
     def __init__(self, *args, **kwargs):
         page.__init__(self, *args, **kwargs)
+        text = tk.Label(self, text = "Her kan brugere redigeres.")
+        text.grid(row = 0, column = 0, padx = 30, pady = 5)
+        btn_getUser = tk.Button(self, text = "Hent chip", command = self.getUser, width=50, height=10, activebackground="green yellow")
+        btn_getUser.grid(row = 0, column = 0, padx = 30, pady = 5)
+        
+    def getUser(self): #Private and public data
+        data = nfcReader.readData()
+        print(data)
+        
+        navnText = tk.Label(self, text = "Fuldt navn")
+        self.navnInput = tk.Entry(self)
+
+        emailText = tk.Label(self, text = "E-mail adresse")
+        self.emailInput = tk.Entry(self)
+        
+        adresseText = tk.Label(self, text = "Adresse")
+        self.adresseInput = tk.Entry(self)
+ 
+        birthdayText = tk.Label(self, text = "Fødselsdato")
+        self.birthdayInput = tk.Entry(self)
+        
+        billedeText = tk.Label(self, text = "Billede til identifikation")
+        self.billedeInput = tk.Entry(self)
+        
+        btn_update = tk.Button(self, text = "Opdater bruger", command=self.updateUser)
+        
+         
+        
+        navnText.grid(row = 1, column = 0, padx = 30, pady = 5)
+        self.navnInput.grid(row = 2, column = 0, padx = 5, pady = 5)
+        
+        emailText.grid(row = 3, column = 0, padx = 30, pady = 5,)
+        self.emailInput.grid(row = 4, column = 0, padx = 5, pady = 5)
+        
+        adresseText.grid(row = 5, column = 0, padx = 5, pady = 5)
+        self.adresseInput.grid(row = 6, column = 0, padx = 5, pady = 5)
+        
+        birthdayText.grid(row = 7, column = 0, padx = 5, pady = 5)
+        self.birthdayInput.grid(row = 8, column = 0, padx = 5, pady = 5)
+        
+        billedeText.grid(row = 9, column = 0, padx = 5, pady = 5)
+        self.billedeInput.grid(row = 10, column = 0, padx = 5, pady = 5)
+        
+        btn_update.grid(row = 11, column = 0, padx = 30, pady = 5)   
+    
+    def updateUser(self):
+        pass
+
+class New_User(page):
+    def __init__(self, *args, **kwargs):
+        page.__init__(self, *args, **kwargs)
+        newUserText = tk.Label(self, text = "Register New User", width=20, font="FreeMono")
         
         navnText = tk.Label(self, text = "Fuldt navn")
         self.navnInput = tk.Entry(self)
@@ -70,43 +127,64 @@ class Edit_User(page):
         billedeText = tk.Label(self, text = "Billede til identifikation")
         self.billedeInput = tk.Entry(self)
         
-        btn_update = tk.Button(self, text = "Opdater bruger")
-        btn_getUser = tk.Button(self, text = "Hent chip")
+        btn_update = tk.Button(self, text = "Opdater bruger", command=self.newUser)
         
-        navnText.grid(row = 0, column = 0, padx = 30, pady = 5)
-        self.navnInput.grid(row = 1, column = 0, padx = 5, pady = 5)
+         
+        newUserText.grid(row = 0, column = 0, padx = 0, pady = 0)
         
-        emailText.grid(row = 2, column = 0, padx = 30, pady = 5,)
-        self.emailInput.grid(row = 3, column = 0, padx = 5, pady = 5)
+        navnText.grid(row = 1, column = 0, padx = 0, pady = 0)
+        self.navnInput.grid(row = 2, column = 0, padx = 5, pady = 5)
         
-        adresseText.grid(row = 4, column = 0, padx = 5, pady = 5)
-        self.adresseInput.grid(row = 5, column = 0, padx = 5, pady = 5)
+        emailText.grid(row = 3, column = 0, padx = 30, pady = 5,)
+        self.emailInput.grid(row = 4, column = 0, padx = 5, pady = 5)
         
-        birthdayText.grid(row = 6, column = 0, padx = 5, pady = 5)
-        self.birthdayInput.grid(row = 7, column = 0, padx = 5, pady = 5)
+        adresseText.grid(row = 5, column = 0, padx = 5, pady = 5)
+        self.adresseInput.grid(row = 6, column = 0, padx = 5, pady = 5)
         
-        billedeText.grid(row = 8, column = 0, padx = 5, pady = 5)
-        self.billedeInput.grid(row = 9, column = 0, padx = 5, pady = 5)
+        birthdayText.grid(row = 7, column = 0, padx = 5, pady = 5)
+        self.birthdayInput.grid(row = 8, column = 0, padx = 5, pady = 5)
         
-        btn_update.grid(row = 10, column = 0, padx = 30, pady = 5)
-        btn_getUser.grid(row = 11, column = 0, padx = 30, pady = 5)    
+        billedeText.grid(row = 9, column = 0, padx = 5, pady = 5)
+        self.billedeInput.grid(row = 10, column = 0, padx = 5, pady = 5)
         
-    def getUser(self): #Private and public data
+        btn_update.grid(row = 11, column = 0, padx = 30, pady = 5)  
+         
+            
+    def newUser(self): #Private and public data
         pass
-    
-    def updateUser(self):
-        pass
-    
-    def newUser(self):
-        pass
-    
+        
 
 class NFC_Reader():
     def __init__(self):
         import nfc
+        self.clf = nfc.ContactlessFrontend()
+        
     
     def readData(self):
-        pass
+        #Check NFC-reader connection
+        try:
+            assert self.clf.open('usb') is True #Determened in cmd by command: "python -m nfc"
+            print(f"Using device: {self.clf}")
+
+        except AssertionError:
+            print("NFC-reader not set up correctly. Try again! (Maybe the error is in the code!)")
+            self.clf.close()  #Test over - New connection will be needed
+            
+        tag = self.clf.connect(rdwr={'on-connect': lambda tag: False})
+        print("Tag found!")
+        print(tag)
+            
+        if not tag.ndef == None:
+            for record in tag.ndef.records:
+                print(record)
+            self.clf.close()
+            return(tag.ndef.records)
+        
+        else: 
+            print(tag.dump())
+            print("Card Carries No Data!\n")
+            self.clf.close()
+            return(None)
     
     def writeData(self):
         pass
@@ -128,6 +206,7 @@ class ServerCommunication():
         pass
 
 if __name__ == "__main__":
+    nfcReader = NFC_Reader()
     base = tk.Tk()
     base.title("Bip Bar")
     main = MainFrame(base)
